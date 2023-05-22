d42ef72f15eff5d8970ea5f97a6f6113f5661f31
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-05-22 23:03:19 +0530

    Added the route for login component

--- timetable-management-system/src/App.jsx
+++ timetable-management-system/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/login";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Route path="/" exact render={(props) => <Login {...props} />} />
+    </Router>
+  );
 }
 
 export default App;
