35ca7c807ac9496a761c2734d56554d82adac663
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-05-22 21:27:04 +0530

    Initialized the connection with MongoDB

--- tt-management-backend/server.js
+++ tt-management-backend/server.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
 const port = 8070;
+
+require("dotenv").config();
+
+MONGODB_URL = process.env.MONGODB_URL;
+
+mongoose.connect(MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB Connection Success!");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
