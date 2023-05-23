9b131a09c4a65cfea75a0082ca56beaba083b66c
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-05-23 21:03:35 +0530

    Implemented axios function

--- timetable-management-system/src/components/Login.jsx
+++ timetable-management-system/src/components/Login.jsx
@@ -1,17 +1,51 @@
+import { useState } from "react";
+import axios from "axios";
+
 export default function Login(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   function proceed(e) {
     e.preventDefault();
+
+    axios
+      .get(`http://localhost:8070/user/${username}/${password}`)
+      .then((res) => {
+        console.log(res.data);
+        redirect(res.data.userType);
+      });
+  }
+
+  function redirect(usrType) {
+    if (usrType === "admin") {
+    } else if (usrType === "") {
+    } else if (usrType === "") {
+    } else {
+      alert("Cannot validate the user type!");
+    }
   }
 
   return (
     <form onSubmit={proceed}>
       User Name
       <br />
-      <input type="email" />
+      <input
+        type="email"
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+        required
+      />
       <br />
       Password
       <br />
-      <input type="password" />
+      <input
+        type="password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        required
+      />
       <br />
       <br />
       <button type="submit">Login</button>
